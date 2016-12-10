948fdf796e9c96d733a9ccef1442fa1bab949e50
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-12-09 19:25:49 -0600

    Added basic logic to send requests out regularly

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "tinyswift",
 	dependencies: [
 		.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
-		.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 1)
+		.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 2)
 	]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,15 +1,61 @@
 import Kitura
 import Foundation
-
+import Dispatch
 import HeliumLogger
 
 HeliumLogger.use()
 
 let router = Router()
 
+let queue = DispatchQueue(label: "clientmsg", attributes: .concurrent)
+let timer = DispatchSource.makeTimerSource(flags: [], queue: queue)
+
+timer.scheduleRepeating(deadline: .now(), interval: .seconds(2))
+
+
+timer.setEventHandler {
+    
+    var request = URLRequest(url: URL(string: "http://localhost:8090")!)
+    
+    
+    request.httpMethod = "POST"
+    request.httpBody = Data()
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.setValue("awesome", forHTTPHeaderField: "Test-Header")
+    
+    let task = URLSession.shared.dataTask(with: request) {
+        data, response, error in
+        
+        // print(response)
+        
+    }
+    
+    task.resume()
+    
+    print("Send a message")
+}
+
+timer.resume()
+
 router.get("/") {
     request, response, next in
     response.send("Hello, World!")
+    next()
+}
+
+
+router.post("/") {
+    request, response, next in
+    
+    request.headers.forEach {
+        print($0)
+    }
+    
+    print(request.userInfo)
+    
+    response.status(.OK).send("OK")
+    
+    
     next()
 }
 
